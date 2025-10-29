1594df97cf964216b28b4b96da9162830be2466f
Miguel Rojo <miguel.rojo.ext@adevinta.com>
2025-10-29 18:46:38 +0100

    Commit current progress.

--- IOT-weather/core/use_cases/metric/add_new_metric.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from core.domain.metric.metric import Metric
-
-@dataclass(frozen=True)
-class AddNewMetric:    
-    def Add(metrict: Metric) -> None:
-        pass
-
--- IOT/core/domain/metric/metric.py
+++ IOT/core/domain/metric/metric.py
@@ -2,13 +2,15 @@ from dataclasses import dataclass
 from core.domain.common.value_objects.timestamp import TimeStamp
 from core.domain.common.value_objects.temperature import Temperature
 from core.domain.common.value_objects.humidity import Humidity
+from core.domain.common.value_objects.device import Device
 
 @dataclass(frozen=True)
 class Metric:
     datetime: TimeStamp
     temp: Temperature
     humidity: Humidity
-    device: str
+    device: Device
     
     def __str__(self) -> str:
-        return print(f"[{self.datetime}][{self.device}]{self.temp}")
+        return print(f"[{self.datetime.value}][{self.device.no}]{self.temp.value}")    
+        
